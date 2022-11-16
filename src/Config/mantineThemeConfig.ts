e7e62cc39c709fe7d5df200b991bb728255ea0c9
aShahriarSazim <ahnaf.shahriar@sazim.io>
2022-11-17 00:34:12 +0600

    Initialize mantine UI
    * Create config file in @/Config for mantine default theme configuration
    * Add mantine provider in main.tsx

---
+++ src/Config/mantineThemeConfig.ts
@@ -0,0 +1,6 @@
+import { MantineThemeOverride } from '@mantine/core';
+
+export const mantineTheme: MantineThemeOverride = {
+	defaultRadius: 0,
+	fontFamily: 'Inter, sans-serif'
+};
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "@/App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { MantineProvider } from '@mantine/core';
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+import { mantineTheme } from '@/Config';
+import App from '@/App';
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+	<React.StrictMode>
+		<MantineProvider theme={mantineTheme} withGlobalStyles withNormalizeCSS>
+			<App />
+		</MantineProvider>
+	</React.StrictMode>
 );
