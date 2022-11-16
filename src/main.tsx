ef4057ef00548f6c8bb972c15088e777e1e175f2
aShahriarSazim <107447519+aShahriarSazim@users.noreply.github.com>
2022-11-17 01:00:01 +0600

    Initialize Redux Toolkit (#2)

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import { MantineProvider } from '@mantine/core';
 
+import { store } from '@/Redux/store';
 import { mantineTheme } from '@/Config';
 import App from '@/App';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
-		<MantineProvider theme={mantineTheme} withGlobalStyles withNormalizeCSS>
-			<App />
-		</MantineProvider>
+		<Provider store={store}>
+			<MantineProvider theme={mantineTheme} withGlobalStyles withNormalizeCSS>
+				<App />
+			</MantineProvider>
+		</Provider>
 	</React.StrictMode>
 );
