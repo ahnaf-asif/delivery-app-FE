c27440bb8572850a8b637bd7ef2026f23be02e7d
aShahriarSazim <ahnaf.shahriar@sazim.io>
2022-11-17 16:24:11 +0600

    created layout with header, footer, navbar

---
+++ src/Layouts/Components/Footer/Footer.styles.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import styled from '@emotion/styled';
+import { Footer } from '@mantine/core';
+
+export const StyledMantineFooter = styled(Footer)`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+`;
