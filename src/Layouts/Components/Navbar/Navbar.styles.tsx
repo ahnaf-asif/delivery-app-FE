d0a1c28cd2c16040bcc6826620e68a23570f1bba
aShahriarSazim <ahnaf.shahriar@sazim.io>
2022-11-22 01:24:10 +0600

    added stuff

--- src/Layouts/Components/Navbar/Navbar.styles.tsx
+++ src/Layouts/Components/Navbar/Navbar.styles.tsx
@@ -3,13 +3,13 @@ import styled from '@emotion/styled';
 import { Drawer, Navbar } from '@mantine/core';
 
 export const StyledNavbar = styled(Navbar)`
-	@media (max-width: 760px) {
+	@media (max-width: 768px) {
 		display: none;
 	}
 `;
 
 export const StyledDrawer = styled(Drawer)`
-	@media (min-width: 761px) {
+	@media (min-width: 769px) {
 		display: none;
 	}
 `;
