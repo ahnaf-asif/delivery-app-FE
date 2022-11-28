6d934bb17a03b9316e56318895a00061d5bbe388
aShahriarSazim <107447519+aShahriarSazim@users.noreply.github.com>
2022-11-29 01:58:13 +0600

    Fixed navbar styles (#8)
    Co-authored-by: Ahnaf Shahriar Asif <37590199+ahnaf-asif@users.noreply.github.com>

--- src/Layouts/Components/Footer/Footer.tsx
+++ src/Layouts/Components/Footer/Footer.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Box } from '@mantine/core';
 
 import { StyledMantineFooter } from './Footer.styles';
 
 const Footer = () => {
-	return <StyledMantineFooter height={60}>footer</StyledMantineFooter>;
+	const { innerWidth } = window;
+	return <Box>{innerWidth <= 768 && <StyledMantineFooter height={60}>footer</StyledMantineFooter>}</Box>;
 };
 
 export default Footer;
